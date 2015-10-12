2a7fd7ebd4d4fc0f5874d44a35b2f2adc47e49c4
Kevin Munroe <kmunroe@eharmony.com>
2015-10-12 10:25:53 -0700

    Changed name to MatchFeedDto

--- api/src/main/java/com/eharmony/services/mymatchesservice/matchfeed/MatchfeedDto.java
+++ api/src/main/java/com/eharmony/services/mymatchesservice/matchfeed/MatchfeedDto.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
-public class MatchfeedDto {
+public class MatchFeedDto {
 
     private String name;
     
@@ -33,7 +33,7 @@ public class MatchfeedDto {
         if (obj.getClass() != getClass()) {
             return false;
         }
-        MatchfeedDto rhs = (MatchfeedDto) obj;
+        MatchFeedDto rhs = (MatchFeedDto) obj;
         return new EqualsBuilder()
                 .append(name, rhs.getName()).isEquals();
     }
