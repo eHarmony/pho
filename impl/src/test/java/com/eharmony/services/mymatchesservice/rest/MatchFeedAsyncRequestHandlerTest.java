647acfa420276ea9d1bd9ba7806e5979f0c7d1f7
Kevin Munroe <kmunroe@eharmony.com>
2015-12-04 18:41:22 -0800

    More HBase fallback test cases.

--- impl/src/test/java/com/eharmony/services/mymatchesservice/rest/MatchFeedAsyncRequestHandlerTest.java
+++ impl/src/test/java/com/eharmony/services/mymatchesservice/rest/MatchFeedAsyncRequestHandlerTest.java
@@ -52,7 +52,7 @@ public class MatchFeedAsyncRequestHandlerTest {
 	}
 	
 	@Test
-	public void testFallback_VoldyNoHBase_NO(){
+	public void testFallback_VoldyNoHbase_NO(){
 		
 		MatchFeedQueryContext queryCtx = MatchFeedQueryContextBuilder.newInstance().build();
 		MatchFeedRequestContext ctx = new MatchFeedRequestContext(queryCtx);
@@ -78,7 +78,7 @@ public class MatchFeedAsyncRequestHandlerTest {
 	}
 	
 	@Test
-	public void testFallback_VoldyEmptyNoHBase_NO(){
+	public void testFallback_VoldyEmptyNoHbase_NO(){
 		
 		MatchFeedQueryContext queryCtx = MatchFeedQueryContextBuilder.newInstance().build();
 		MatchFeedRequestContext ctx = new MatchFeedRequestContext(queryCtx);
@@ -112,6 +112,8 @@ public class MatchFeedAsyncRequestHandlerTest {
 		Set<MatchDataFeedItemDto> feedSet = new HashSet<MatchDataFeedItemDto>();
 		feedSet.add(new MatchDataFeedItemDto());
 		feedsByStatusGroup.put(MatchStatusGroupEnum.NEW, feedSet);
+		feedsByStatusGroup.put(MatchStatusGroupEnum.ARCHIVE, feedSet);
+		feedsByStatusGroup.put(MatchStatusGroupEnum.COMMUNICATION, feedSet);
 		
 		LegacyMatchDataFeedDto legacy = new LegacyMatchDataFeedDto();
 		Map<String, Map<String, Map<String, Object>>> matches = new HashMap<>();
@@ -129,6 +131,37 @@ public class MatchFeedAsyncRequestHandlerTest {
 		boolean shouldFallback = handler.shouldFallbackToHBase(ctx);
 		
 		assertTrue(shouldFallback);
+	}
+	
+	@Test
+	public void testFallback_VoldyEmptyAndHbaseEmpty_NO(){
+		
+		MatchFeedQueryContext queryCtx = MatchFeedQueryContextBuilder.newInstance().build();
+		MatchFeedRequestContext ctx = new MatchFeedRequestContext(queryCtx);
+
+		Map<MatchStatusGroupEnum, Set<MatchDataFeedItemDto>> feedsByStatusGroup 
+													= ctx.getHbaseFeedItemsByStatusGroup();		
+
+		feedsByStatusGroup.put(MatchStatusGroupEnum.NEW, null);
+		feedsByStatusGroup.put(MatchStatusGroupEnum.ARCHIVE, null);
+		feedsByStatusGroup.put(MatchStatusGroupEnum.COMMUNICATION, null);
+		
+		LegacyMatchDataFeedDto legacy = new LegacyMatchDataFeedDto();
+		Map<String, Map<String, Map<String, Object>>> matches = new HashMap<>();
+		legacy.setMatches(matches);
+		
+		long userId = 62837673;
+		
+		ctx.setLegacyMatchDataFeedDtoWrapper(new LegacyMatchDataFeedDtoWrapper(userId));
+		ctx.getLegacyMatchDataFeedDtoWrapper().setLegacyMatchDataFeedDto(legacy);
+		ctx.getLegacyMatchDataFeedDtoWrapper().setFeedAvailable(true);
+		
+		
+		MatchFeedAsyncRequestHandler handler = new MatchFeedAsyncRequestHandler();
+		
+		boolean shouldFallback = handler.shouldFallbackToHBase(ctx);
+		
+		assertTrue(!shouldFallback);
 	}
 	
 	@Test
@@ -161,5 +194,38 @@ public class MatchFeedAsyncRequestHandlerTest {
 		boolean shouldFallback = handler.shouldFallbackToHBase(ctx);
 		
 		assertTrue(!shouldFallback);
+	}
+	
+	@Test
+	public void testFallback_VoldyHasDataButErrorAndHbase_YES(){
+		
+		MatchFeedQueryContext queryCtx = MatchFeedQueryContextBuilder.newInstance().build();
+		MatchFeedRequestContext ctx = new MatchFeedRequestContext(queryCtx);
+
+		Map<MatchStatusGroupEnum, Set<MatchDataFeedItemDto>> feedsByStatusGroup 
+													= ctx.getHbaseFeedItemsByStatusGroup();		
+		Set<MatchDataFeedItemDto> feedSet = new HashSet<MatchDataFeedItemDto>();
+		feedSet.add(new MatchDataFeedItemDto());
+		feedsByStatusGroup.put(MatchStatusGroupEnum.NEW, feedSet);
+		
+		long matchId = 11790420914L;
+		LegacyMatchDataFeedDto legacy = new LegacyMatchDataFeedDto();
+		Map<String, Map<String, Map<String, Object>>> matches = new HashMap<>();
+		matches.put(String.valueOf(matchId), new HashMap<String, Map<String, Object>>());
+		legacy.setMatches(matches);
+		
+		long userId = 62837673;
+		
+		ctx.setLegacyMatchDataFeedDtoWrapper(new LegacyMatchDataFeedDtoWrapper(userId));
+		ctx.getLegacyMatchDataFeedDtoWrapper().setLegacyMatchDataFeedDto(legacy);
+		ctx.getLegacyMatchDataFeedDtoWrapper().setFeedAvailable(false);
+		ctx.getLegacyMatchDataFeedDtoWrapper().setError(new Exception());
+		
+		
+		MatchFeedAsyncRequestHandler handler = new MatchFeedAsyncRequestHandler();
+		
+		boolean shouldFallback = handler.shouldFallbackToHBase(ctx);
+		
+		assertTrue(shouldFallback);
 	}
 }
