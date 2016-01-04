9e5b440b42611eb62d7410bc9c0c7ded1389d30b
gwang <gwang@eharmony.com>
2016-01-04 09:51:16 -0800

    import organize.

--- impl/src/main/java/com/eharmony/services/mymatchesservice/event/MatchQueryEventService.java
+++ impl/src/main/java/com/eharmony/services/mymatchesservice/event/MatchQueryEventService.java
@@ -3,7 +3,6 @@ package com.eharmony.services.mymatchesservice.event;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.annotation.Resource;
 
