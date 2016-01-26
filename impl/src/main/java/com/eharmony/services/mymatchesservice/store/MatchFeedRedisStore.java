c3945e6e86bd883212b22cac5bf5d80b42819aaa
gwang <gwang@eharmony.com>
2016-01-26 12:43:53 -0800

    SFT-18887 combine matches in the feed

--- impl/src/main/java/com/eharmony/services/mymatchesservice/store/MatchFeedRedisStore.java
+++ impl/src/main/java/com/eharmony/services/mymatchesservice/store/MatchFeedRedisStore.java
@@ -1,42 +1,62 @@
 package com.eharmony.services.mymatchesservice.store;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
-import com.eharmony.services.mymatchesservice.rest.MatchFeedQueryContext;
+import com.eharmony.services.mymatchesservice.service.BasicStoreFeedRequestContext;
+import com.eharmony.services.mymatchesservice.service.RedisStoreFeedResponse;
+import com.eharmony.services.mymatchesservice.service.RedisStoreFeedService;
 import com.eharmony.services.mymatchesservice.store.serializer.LegacyMatchDataFeedDtoSerializer;
 import com.google.common.base.Preconditions;
 
-public class MatchFeedRedisStore {
-	private static final Logger log = LoggerFactory.getLogger(MatchFeedRedisStore.class);
-	private RedisTemplate<String, String> redisMatchDataTemplate;
-	private LegacyMatchDataFeedDtoSerializer matchDataFeedSerializer;
+import rx.Observable;
 
-	public MatchFeedRedisStore(RedisTemplate<String, String> redisMatchDataTemplate,
-			LegacyMatchDataFeedDtoSerializer matchDataFeedSerializer) {
+public class MatchFeedRedisStore implements RedisStoreFeedService{
+    private static final Logger log = LoggerFactory.getLogger(MatchFeedRedisStore.class);
+    private RedisTemplate<String, String> redisMatchDataTemplate;
+    private LegacyMatchDataFeedDtoSerializer matchDataFeedSerializer;
 
-		Preconditions.checkNotNull(redisMatchDataTemplate);
+    public MatchFeedRedisStore(RedisTemplate<String, String> redisMatchDataTemplate,
+            LegacyMatchDataFeedDtoSerializer matchDataFeedSerializer) {
 
-		Preconditions.checkNotNull(matchDataFeedSerializer);
+        Preconditions.checkNotNull(redisMatchDataTemplate);
 
-		this.redisMatchDataTemplate = redisMatchDataTemplate;
-		this.matchDataFeedSerializer = matchDataFeedSerializer;
-	}
+        Preconditions.checkNotNull(matchDataFeedSerializer);
 
-	public LegacyMatchDataFeedDtoWrapper getMatchesSafe(MatchFeedQueryContext queryContext) {
-		String userid = String.valueOf(queryContext.getUserId());
-		LegacyMatchDataFeedDtoWrapper resultWrapper = new LegacyMatchDataFeedDtoWrapper(queryContext.getUserId());
-		try {
-			HashOperations<String, String, String> hashOps = redisMatchDataTemplate.opsForHash();
-			hashOps.entries(userid).forEach((key, value) -> {
-				resultWrapper.setLegacyMatchDataFeedDto(matchDataFeedSerializer.fromJson(value));
-			});
-		} catch (Exception exp) {
-			log.warn("Error while getting feed for user{} from Redis", userid, exp);
-		}
+        this.redisMatchDataTemplate = redisMatchDataTemplate;
+        this.matchDataFeedSerializer = matchDataFeedSerializer;
+    }
 
-		return resultWrapper;
-	}
+    public Observable<RedisStoreFeedResponse> getUserMatchesSafe(BasicStoreFeedRequestContext request) {
+        String userid = String.valueOf(request.getMatchFeedQueryContext().getUserId());
+        RedisStoreFeedResponse response = new RedisStoreFeedResponse();
+        LegacyMatchDataFeedDto feedDto = response.getRedisStoreFeedDto();
+        try {
+            HashOperations<String, String, String> hashOps = redisMatchDataTemplate.opsForHash();
+            Map<String, String> hashEntries = hashOps.entries(userid);
+            hashEntries.forEach((key, value) -> {
+                
+                LegacyMatchDataFeedDto matchDto = matchDataFeedSerializer.fromJson(value);
+                if (feedDto == null) {
+                    response.setRedisStoreFeedDto(matchDto);
+                } else {
+                    Map<String, Map<String, Object>> singleUpdatedMatch = matchDto.getMatches().get(key);
+                    feedDto.getMatches().put(key, singleUpdatedMatch);
+                }
+            });
+            if (feedDto != null) {
+                feedDto.setTotalMatches(hashEntries.size());
+            }
+        } catch (Exception exp) {
+            log.warn("Error while getting feed for user{} from Redis", userid, exp);
+            //re throw exception so the down stream observer can deal with it.
+            throw exp;
+        }
+
+        return Observable.just(response);
+    }
 }
