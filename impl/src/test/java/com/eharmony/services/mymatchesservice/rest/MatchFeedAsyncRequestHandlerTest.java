6818d8f72f4a9a4e61397344cb367e90580db7bc
Kevin Munroe <kmunroe@eharmony.com>
2016-02-10 14:43:10 -0800

    Added Redis merge strategy to getTeaserMatches

--- impl/src/test/java/com/eharmony/services/mymatchesservice/rest/MatchFeedAsyncRequestHandlerTest.java
+++ impl/src/test/java/com/eharmony/services/mymatchesservice/rest/MatchFeedAsyncRequestHandlerTest.java
@@ -1,21 +1,61 @@
 package com.eharmony.services.mymatchesservice.rest;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import javax.annotation.Resource;
+import javax.ws.rs.container.AsyncResponse;
+
 import org.junit.Test;
+import org.powermock.reflect.Whitebox;
 
 import com.eharmony.datastore.model.MatchDataFeedItemDto;
+import com.eharmony.services.mymatchesservice.event.EventConstant;
+import com.eharmony.services.mymatchesservice.event.MatchQueryEventService;
+import com.eharmony.services.mymatchesservice.service.RedisStoreFeedService;
 import com.eharmony.services.mymatchesservice.store.LegacyMatchDataFeedDto;
 import com.eharmony.services.mymatchesservice.store.LegacyMatchDataFeedDtoWrapper;
 import com.eharmony.services.mymatchesservice.util.MatchStatusGroupEnum;
+import com.eharmony.singles.common.status.MatchStatus;
 
 public class MatchFeedAsyncRequestHandlerTest {
-
+	
+	@Test
+	public void testGetTeaserMatches(){
+		
+		long userId = 12345L;
+		
+		Set<String> statusSet = new HashSet<String>();
+		statusSet.add(MatchStatus.NEW.name());
+		
+		MatchFeedQueryContext queryCtx = MatchFeedQueryContextBuilder.newInstance().setAllowedSeePhotos(true)
+                .setPageSize(100)   // For phase 1 setting 100 as the default number of records to fetch from HBASE. In V2, there will be DAO service for this.
+                .setStartPage(1)  //There will be no pagination. There will be only one page and the resultSize param will decide how many items it consists of.
+                .setStatuses(statusSet)
+                .setUserId(userId)
+                .setTeaserResultSize(100)        // This is the number of results to be returned back to the client/user.              
+                .build();
+		
+		MatchFeedAsyncRequestHandler handler = new MatchFeedAsyncRequestHandler();	
+		
+        Map<String,String> eventContextInfo = new HashMap<String,String>();
+        
+    	eventContextInfo.put(EventConstant.USER_AGENT, "someUserAgent");
+    	eventContextInfo.put(EventConstant.PLATFORM, "somePlatform");
+		
+    	Whitebox.setInternalState(handler, "redisMergeMode", true);
+    	Whitebox.setInternalState(handler, "matchQueryEventService", mock(MatchQueryEventService.class));
+    	Whitebox.setInternalState(handler, "redisStoreFeedService", mock(RedisStoreFeedService.class));
+ 
+		AsyncResponse response = mock(AsyncResponse.class);
+		handler.getTeaserMatchesFeed(queryCtx, response, eventContextInfo);
+	}
+	
 	@Test
 	public void testFallback_noVoldyNoHbase_NO(){
 		
