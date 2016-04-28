6f54ed2ee0a8b4056bbcd7c72d6d1816c34edbc4
Kevin Munroe <kmunroe@eharmony.com>
2016-04-27 18:10:41 -0700

    SFT-18888 - upgraded to Hibernate 4 template, dropped DAOs.

--- impl/src/main/java/com/eharmony/services/mymatchesservice/store/MatchDataFeedSORAStore.java
+++ impl/src/main/java/com/eharmony/services/mymatchesservice/store/MatchDataFeedSORAStore.java
@@ -1,49 +1,50 @@
 package com.eharmony.services.mymatchesservice.store;
 
+import java.util.List;
+
 import javax.annotation.Resource;
-import javax.transaction.Transactional;
+//import javax.transaction.Transactional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.orm.hibernate4.HibernateTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.eharmony.services.mymatchesservice.store.dao.MatchDao;
-import com.eharmony.services.mymatchesservice.store.dao.MatchSummaryDao;
 import com.eharmony.services.mymatchesservice.store.data.MatchDo;
 import com.eharmony.services.mymatchesservice.store.data.MatchSummaryDo;
 
 @Component("soraStore")
 public class MatchDataFeedSORAStore {
-
-	@Resource
-	private MatchDao matchDao;
-	
-	@Resource
-	private MatchSummaryDao matchSummaryDao;
 		
 	private static final Logger logger = LoggerFactory.getLogger(MatchDataFeedSORAStore.class);
+
+	@Resource
+	private HibernateTemplate hibernateTemplate;
 	
-    @Transactional
+	@Transactional(readOnly=true)
 	public MatchDo getMatch(long matchId){
-    	
-    	try{
-    		return matchDao.findByPrimaryKey(matchId);
-    		
-    	}catch(Exception ex){
-    		logger.warn("Error getting EHMATCH record {}: {}", matchId, ex.getMessage());
-    		return null;
-    	}
-    }
-    
-    @Transactional
+		
+		List<?> matches = hibernateTemplate.find("from MatchDo where matchId = ?", matchId);
+		
+		logger.debug("searched EHMATCH for matchId {}, found {} matches.", matchId, matches.size());
+		if(matches.size() > 0){
+			return (MatchDo)matches.get(0);
+		}
+		
+		return null;
+	}
+	
+	@Transactional(readOnly=true)
 	public MatchSummaryDo getMatchSummary(long userId, long matchId){
-    	
-    	try{
-    		return matchSummaryDao.findMatchSummaryByUserAndMatch(userId, matchId);
-    		
-    	}catch(Exception ex){
-    		logger.warn("Error getting MATCH_SUMMARIES record for user {} match {}: {}", userId, matchId, ex.getMessage());
-    		return null;
-    	}
-    }
+		
+		List<?> matches = hibernateTemplate.find("from MatchSummaryDo where userId = ? and matchId = ?", userId, matchId);
+		logger.debug("searched MATCH_SUMMARIES for userId {} matchId {}, found {} matches.", userId, matchId, matches.size());
+		
+		if(matches.size() > 0){
+			return (MatchSummaryDo)matches.get(0);
+		}
+		
+		return null;
+	}
 }
